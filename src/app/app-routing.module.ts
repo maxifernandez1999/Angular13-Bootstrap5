1dc93e6371fdb642d097b4aed422d0605c735d55
maxifernandez1999 <fmaximiliano443@gmail.com>
2022-01-21 16:52:24 -0300

    nuevos cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
+import { ContainerComponent } from './layout/container/container.component';
+import { GridComponent } from './layout/grid/grid.component';
+import { BackgroundComponent } from './utilities/background/background.component';
+import { BorderComponent } from './utilities/border/border.component';
 
 const routes: Routes = [
-  { path: 'button', component: ButtonComponent }
+  { path: 'button', component: ButtonComponent },
+  { path: 'container', component: ContainerComponent },
+  { path: 'grid', component: GridComponent },
+  { path: 'background', component: BackgroundComponent },
+  { path: 'border', component: BorderComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ButtonComponent } from './components/button/button.component';
+import { ContainerComponent } from './layout/container/container.component';
+import { GridComponent } from './layout/grid/grid.component';
+import { BackgroundComponent } from './utilities/background/background.component';
+import { BorderComponent } from './utilities/border/border.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    ContainerComponent,
+    GridComponent,
+    BackgroundComponent,
+    BorderComponent
   ],
   imports: [
     BrowserModule,
